8527984c79adfe418ee9ab7c1089577dadab4298
DandushSmith <dandushsmith@gmail.com>
2024-02-03 20:59:53 +0200

    server refactor

---
+++ seeder/codeBlocks.js
@@ -0,0 +1,25 @@
+const initCodeBlockData = async () => {
+  try {
+    const count = await CodeBlock.countDocuments();
+    if (count === 0) {
+      await CodeBlock.insertMany(codeBlocks);
+      console.log("Initial code blocks inserted into the database.");
+    } else {
+      console.log(
+        "Code blocks already exist in the database. Skipping initialization."
+      );
+    }
+  } catch (error) {
+    console.error("Error inserting code blocks:", error);
+    process.exit(1);
+  }
+};
+
+const run = async () => {
+  // connect to mongo
+  // initCodeBlockData
+
+  console.log("in run");
+};
+
+run();
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 require("dotenv").config({
   path: `${__dirname}/.env`,
 });
-const {
-  connectToMongo,
-  initCodeBlockData,
-  CodeBlock,
-} = require("./config/mongo");
+const { connectToMongo, CodeBlock } = require("./config/mongo");
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -16,43 +12,53 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
 
-connectToMongo();
-initCodeBlockData();
+// connectToMongo();
+// initCodeBlockData();
 
 app.use(cors());
 app.use("/codeBlock", router);
 
-const io = socketIO(server, {
-  cors: {
-    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL2],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`Socket connection is running with ${socket.id}`);
-
-  socket.on("join_room", (room) => {
-    const participants = io.sockets.adapter.rooms.get(room);
-    if (participants) {
-      socket.emit("not_first_user");
-    }
-    socket.join(room);
-  });
-
-  socket.on("edit", async (title, code, room) => {
-    try {
-      const updatedCode = await CodeBlock.findOneAndUpdate(
-        { title },
-        { code },
-        { new: true }
-      );
-      socket.broadcast.to(room).emit("edited", updatedCode.code);
-    } catch (error) {
-      console.error("Error updating code:", error);
-    }
-  });
-});
-
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+const initServer = async () => {
+  // await connect to mongoo
+  // define socket
+  // add listern
+  await connectToMongo();
+
+  const io = socketIO(server, {
+    cors: {
+      //TODO: check cors
+      origin: [process.env.CLIENT_URL, process.env.CLIENT_URL2],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`Socket connection is running with ${socket.id}`);
+
+    socket.on("join_room", (room) => {
+      const participants = io.sockets.adapter.rooms.get(room);
+      if (participants) {
+        socket.emit("not_first_user");
+      }
+      socket.join(room);
+    });
+
+    socket.on("edit", async (title, code, room) => {
+      try {
+        const updatedCode = await CodeBlock.findOneAndUpdate(
+          { title },
+          { code },
+          { new: true }
+        );
+        socket.broadcast.to(room).emit("edited", updatedCode.code);
+      } catch (error) {
+        console.error("Error updating code:", error);
+      }
+    });
+  });
+};
+
+initServer();
